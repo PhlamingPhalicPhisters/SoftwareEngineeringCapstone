a446d0b44f90f016c4d63fe105f683294f4e1785
PhlamingPhalicPhisters <geo300@comcast.net>
2018-02-22 11:17:16 -0600

    Merge branch 'master' of https://github.com/PhlamingPhalicPhisters/SoftwareEngineeringCapstone
    # Conflicts:
    #	index.html
    #	js/client.js
    #	js/game.js

--- js/game.js
+++ js/game.js
@@ -11,7 +11,8 @@ Game.init = function(){
 Game.preload = function() {
     game.load.tilemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.spritesheet('tileset', 'assets/map/tilesheet.png',32,32);
-    game.load.image('sprite','assets/sprites/sprite.png'); // this will be the sprite of the players
+    //game.load.image('sprite','assets/sprites/sprite.png'); // this will be the sprite of the players
+    game.load.image('sprite', 'assets/sprites/knuck.gif');
 };
 
 Game.create = function(){
@@ -22,19 +23,48 @@ Game.create = function(){
     for(var i = 0; i < map.layers.length; i++) {
         layer = map.createLayer(i);
     }
+    //Game.physics.enable(p);
+
+    //Game.physics.arcade.gravity.y = 250;
     layer.inputEnabled = true; // Allows clicking on the map
     Client.askNewPlayer();
+    layer.events.onInputUp.add(Game.getCoordinates, this);
 };
 
-Client.askNewPlayer = function(){
-    Client.socket.emit('newplayer');
-}
-
-Game.addNewPlayer = function(id,x,y){
-    Game.playerMap[id] = game.add.sprite(x,y,'sprite');
-};
+/*Game.addNewPlayer = function(id,x,y){
+    Game.playerMap[id] = game.add.sprite(x-32,y-32,'sprite');
+};*/
 
 Game.removePlayer = function(id){
     Game.playerMap[id].destroy();
     delete Game.playerMap[id];
+};
+
+Game.getCoordinates = function(layer, pointer) {
+    Client.sendClick(pointer.worldX, pointer.worldY);
+};
+
+Game.movePlayer = function(id, x, y) {
+    x = x - 32;
+    y = y - 32;
+    var player = Game.playerMap[id];
+    var distance = Phaser.Math.distance(player.x, player.y, x, y);
+    var duration = distance * 1;
+    game.tweens.remove(player.tween);
+    var tween = game.add.tween(player);
+    player.tween = tween;
+    tween.to({x: x, y: y}, duration);
+    tween.start();
+};
+
+//Game.someGroup = Game.add.group();
+
+Game.addNewPlayer = function(id,x,y){
+    Game.playerMap[id] = game.add.sprite(x,y,'sprite');
+    Game.physics.arcade.enable(Game.playerMap[id]);
+    Game.playerMap[id].enableBody = true;
+    //Game.playerMap[id].tween;
+    //Game.playerMap[id].body.immovable = true;
+    //Game.someGroup.add(Game.playerMap[id]);
+    //Game.physics.arcade.collide(Game.playerMap[id], Game.someGroup);
 };
