f24b264d1046230b777b5566eba74ae719050f3b
PhlamingPhalicPhisters <geo300@comcast.net>
2018-03-06 11:11:17 -0600

    fixed delayed inout issue by changing package.json and doing npm install

--- js/game.js
+++ js/game.js
@@ -10,7 +10,7 @@ Game.init = function(){
 
 Game.preload = function() {
     game.load.tilemap('map', 'assets/map/example_map.json', null, Phaser.Tilemap.TILED_JSON);
-    game.load.spritesheet('tileset', 'assets/map/tilesheet.png',32,32);
+    game.load.spritesheet('tileset', 'assets/map/tilesheet.png',64,64);
     //game.load.image('sprite','assets/sprites/sprite.png'); // this will be the sprite of the players
     game.load.image('sprite', 'assets/sprites/knuck.gif');
 };
